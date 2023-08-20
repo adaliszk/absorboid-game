20ee5770b1538e6b7c0a371395b1044b7bf40a97
Ádám Liszkai <adam@liszkai.dev>
2023-08-20 02:49:48 +0200

    ci: Fix shell indent style

--- setup-git-version.sh
+++ setup-git-version.sh
@@ -2,7 +2,7 @@
 
 GDSCRIPT=$(
 	cat <<-EOF
-        const BRANCH = "$(git rev-parse --abbrev-ref HEAD)"
+		const BRANCH = "$(git rev-parse --abbrev-ref HEAD)"
 		const HASH = "#$(git rev-parse --short HEAD)"
 		const TAG = "$(git describe --tags --exact-match 2>/dev/null)"
 	EOF
