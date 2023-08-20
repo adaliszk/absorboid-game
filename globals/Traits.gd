5f70c51e41f4b5420bdf888e83948fa286831273
Ádám Liszkai <adam@liszkai.dev>
2023-08-20 15:59:03 +0200

    fix(traits): Check configuration warning existence

--- globals/Traits.gd
+++ globals/Traits.gd
@@ -8,9 +8,8 @@ static func get_children_configuration_warnings(node: Node) -> PackedStringArray
 
 	var shared_warnings: PackedStringArray = []
 	for child in node.get_children(true):
-		if child is Control:
+		if child is Control and "_get_configuration_warnings" in child:
 			var warnings = child._get_configuration_warnings()
-			if warnings.size() > 0:
-				shared_warnings.append_array(warnings)
+			shared_warnings.append_array(warnings)
 
 	return shared_warnings
