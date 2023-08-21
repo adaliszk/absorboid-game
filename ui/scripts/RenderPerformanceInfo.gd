dd09ffef0159fa0c3999e6af8d0f880cb4655678
Ádám Liszkai <adam@liszkai.dev>
2023-08-21 11:24:16 +0200

    feat: Add Pause menu and behaviour

--- ui/scripts/RenderPerformanceInfo.gd
+++ ui/scripts/RenderPerformanceInfo.gd
@@ -7,19 +7,12 @@ func _process(_delta):
 	var mem_used = Performance.get_monitor(Performance.MEMORY_STATIC)
 	var fps = Performance.get_monitor(Performance.TIME_FPS)
 
-	var mem_snapped = snapped(mem_used / 1048576, 0.01)
-	var mem_unit = "MB"
-	if mem_snapped <= 1.0:
-		mem_snapped = snapped(mem_used / 1024, 0.01)
-		mem_unit = "kB"
-
 	self.text = (
-		"CPU: %sms   GPU: %sms   MEM: %s%s   FPS: %s"
+		"%s/%sMB (%s/%sms)"
 		% [
+			fps,
+			snapped(mem_used / 1048576, 0.01),
 			snapped(cpu_time * 1000, 0.01),
 			snapped(gpu_time * 1000, 0.01),
-			mem_snapped,
-			mem_unit,
-			fps
 		]
 	)
