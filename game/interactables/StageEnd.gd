8305dddd52015e2442b00ca488a26c6f2d95caaa
Ádám Liszkai <adam@liszkai.dev>
2023-08-21 05:25:50 +0200

    refactor: Move TileMapGizmo into Traits

---
+++ game/interactables/StageEnd.gd
@@ -0,0 +1,26 @@
+@tool
+extends Area2D
+
+
+# region: Lifecycle
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		update_configuration_warnings()
+
+
+func _enter_tree() -> void:
+	if Engine.is_editor_hint():
+		update_configuration_warnings()
+
+
+func _notification(what):
+	match what:
+		NOTIFICATION_PARENTED, NOTIFICATION_UNPARENTED:
+			update_configuration_warnings()
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	return Traits.get_children_configuration_warnings(self)
+
+# endregion
