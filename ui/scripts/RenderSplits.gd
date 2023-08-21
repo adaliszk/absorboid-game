761b7da35db37322dd83c5fe0fb133b47d349c53
Ádám Liszkai <adam@liszkai.dev>
2023-08-21 08:10:39 +0200

    refactor: Update split calculation and Results

--- ui/scripts/RenderSplits.gd
+++ ui/scripts/RenderSplits.gd
@@ -6,8 +6,26 @@ func _ready() -> void:
 	var index = 0
 	for split in Game.level_data["splits"]:
 		var split_name = Game.split_names[index]
-		var label = Label.new()
-		label.add_theme_color_override("font_color", Color(0, 0, 0, 1))
-		label.text = "%s: %s" % [split_name, Utils.format_time(split)]
-		add_child(label)
+		var split_item = HBoxContainer.new()
+		split_item.add_child(_get_label(split_name))
+		split_item.add_child(_get_vspacer())
+		split_item.add_child(_get_label(Utils.format_time(split)))
+		add_child(split_item)
+		add_child(HSeparator.new())
 		index += 1
+
+
+
+func _get_label(text: String) -> Label:
+	var label = Label.new()
+	label.add_theme_color_override("font_color", Color(0, 0, 0, 1))
+	label.text = text
+	return label
+
+
+func _get_vspacer() -> VSeparator:
+	var vspacer = VSeparator.new()
+	vspacer.add_theme_stylebox_override("separator", StyleBoxEmpty.new())
+	vspacer.size_flags_horizontal = SIZE_EXPAND
+	return vspacer
+
