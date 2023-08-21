d7d2d22a41eea0d2f143540474d70eb559a14d31
Ádám Liszkai <adam@liszkai.dev>
2023-08-21 21:06:42 +0200

    fix: Remove keyboard focus from PauseMenu

--- ui/components/PauseMenu/PauseMenuController.gd
+++ ui/components/PauseMenu/PauseMenuController.gd
@@ -1,5 +1,7 @@
 extends Control
 
+signal drawer_toggled(state: bool)
+
 
 @export var opened: bool = false:
 	set(value):
@@ -18,14 +20,22 @@ var drawer_position: float
 func _ready() -> void:
 	drawer_position = -drawer.size.x
 	drawer.position.x = drawer_position
+	drawer_toggled.connect(_drawer_toggle)
 
 
 func _process(_delta) -> void:
 	drawer.position.x = lerp(drawer.position.x, drawer_position, 0.2)
+	if drawer.position.x <= -drawer.size.x and drawer.visible:
+		drawer_toggled.emit(opened)
 
 
 func _exit_tree():
 	get_tree().paused = false
+
+
+func _drawer_toggle(state: bool) -> void:
+	Log.debug("visible:%s" % state)
+	drawer.visible = state
 
 # endregion
 
@@ -34,6 +44,8 @@ func _exit_tree():
 func toggle() -> void:
 	opened = not opened
 	Log.info("opened:%s" % opened)
+	if opened:
+		drawer.visible = true
 
 
 func render() -> void:
